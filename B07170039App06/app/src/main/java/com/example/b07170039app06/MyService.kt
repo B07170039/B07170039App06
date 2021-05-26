211cec8ca192375b024f89d305cd1cd1649d015f
B07170039 <kspencer789654@gmail.com>
2021-05-26 11:40:04 +0800

    B07170039App06

---
+++ B07170039App06/app/src/main/java/com/example/b07170039app06/MyService.kt
@@ -0,0 +1,56 @@
+package com.example.b07170039app06
+
+import android.app.Service
+import android.content.Intent
+import android.os.Bundle
+import android.os.IBinder
+import java.util.concurrent.ThreadPoolExecutor
+
+class MyService : Service() {
+
+    companion object{
+        var flag:Boolean = false
+    }
+
+    private var h=0
+    private var m=0
+    private var s=0
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the vommunication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent,flags: Int, startid: Int): Int {
+        flag = intent.getBooleanExtra("flag",false)
+        object :Thread(){
+            override fun run() {
+                while (flag){
+                    try {
+                        Thread.sleep(1000)
+                    }catch (e:InterruptedException){
+                        e.printStackTrace()
+                    }
+                    s++
+                    if (s>=60){
+                        s=0
+                        m++
+                        if(m>=60){
+                            m=0
+                            h++
+                        }
+                    }
+                    val intent = Intent("MyMessage")
+                    val bundle = Bundle()
+                    bundle.putInt("H",h)
+                    bundle.putInt("M",m)
+                    bundle.putInt("S",s)
+                    intent.putExtras(bundle)
+                    sendBroadcast((intent))
+                }
+            }
+        }.start()
+
+
+        return Service.START_STICKY //Service 終止後不再重啟
+    }
+}
